1b12b48b488f9486452d80a29e9b4446436ce61a
nicovill90 <nicovillalonga90@hotmail.com>
2016-12-20 19:47:29 -0300

    Add nav router

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -4,13 +4,9 @@ var Navigation = require('Navigation');
 var Main = (props) => {
   return (
     <div>
-      <div>
-        <div>
-          <Navigation/>
-          <p>Main.jsx Rendered</p>
-          {props.children}
-        </div>
-      </div>
+      <Navigation/>
+      <p>Main.jsx Rendered</p>
+      {props.children}
     </div>
   );
 }

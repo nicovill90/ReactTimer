03b54563e6201217ab397b03a7f6255e4bdc2328
nicovill90 <nicovillalonga90@hotmail.com>
2016-12-21 19:22:51 -0300

    Countdown component, style, test

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,11 +1,9 @@
 var React = require('react');
 
-var Timer = (props) => {
-  return (
-    <div>
-      <p>Timer.jsx Rendered</p>
-    </div>
-  );
-}
+var Timer = React.createClass({
+  render: function () {
+    return <p>Timer.jsx</p>;
+  }
+});
 
 module.exports = Timer;
